f5425acadc7cd225665b99c8fb40d80081268824
il <ilya.mv@gmail.com>
2023-02-14 20:32:19 -0500

    Compliting day 3

--- udemy/day_3/day_3Ex_OddorEven.py
+++ udemy/day_3/day_3Ex_OddorEven.py
@@ -1,8 +1,6 @@
-number = int(input("which number do you want to check ?"))
+number = int(input("which number do you want to check?"))
 
 if number % 2 == 0:
-
-    print("this is even number ")
-
-else:
     print("this is even number")
+else:
+    print("this is odd number")
