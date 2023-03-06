4b3588da02be12f93a6283abddad423b74e2d925
il <ilya.mv@gmail.com>
2023-03-06 02:01:26 -0500

    Completed Leap year .

---
+++ udemy/day_3/day_3EX_PizzaOrderPractice.py
@@ -0,0 +1,13 @@
+print("Welcome to Python Pizza Deliveries!")
+
+
+size = input("What size pizza do you want ? S , M , or L ")
+
+
+
+
+#print("Do you want pepperoni ? Y or N")
+
+#print("Do you want extra cheese ? Y or N")
+
+#print(" Your final bill is : ")
