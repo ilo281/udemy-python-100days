7816d3ae61e9d1507592551483e74a943729bbd0
il <ilya.mv@gmail.com>
2023-03-13 04:07:39 -0400

    Updated progress .

--- udemy/day_3/day_3EX_PizzaOrderPractice.py
+++ udemy/day_3/day_3EX_PizzaOrderPractice.py
@@ -3,11 +3,14 @@ print("Welcome to Python Pizza Deliveries!")
 
 size = input("What size pizza do you want ? S , M , or L ")
 
+L=15
 
+print(size)
 
+#add_pepperoni = input("Do you want pepperoni ? Y or N")
 
-#print("Do you want pepperoni ? Y or N")
+#extra_cheese = input("Do you want extra cheese ? Y or N")
 
-#print("Do you want extra cheese ? Y or N")
+# bill =
 
 #print(" Your final bill is : ")
