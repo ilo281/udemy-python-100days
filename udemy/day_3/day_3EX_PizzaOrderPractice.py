d21c55ef5edb93db1801926eb929c47c784b3622
il <ilya.mv@gmail.com>
2023-03-21 20:59:19 -0400

    Pizza_Calculator

--- udemy/day_3/day_3EX_PizzaOrderPractice.py
+++ udemy/day_3/day_3EX_PizzaOrderPractice.py
@@ -1,38 +1,25 @@
 
 print("Welcome to Python Pizza Deliveries!")
 
-bill :int = 0
+bill = 0
 
 size = input("What size pizza do you want ? S , M , or L ")
+add_pepperoni = input("Do you want pepperoni ? Y or N ")
+extra_cheese = input("Do you want extra cheese ? Y or N ")
 
-if size != "S":
-  pass
-else:
+if size == "S":
     bill = 15
-    if  size != "M":
-        bill = 20
+    if add_pepperoni == "Y":
+        bill += 2
+if size == "M":
+    bill = 20
+    if add_pepperoni == "Y":
+        bill += 3
+if size == "L":
+    bill = 25
+    if add_pepperoni == "Y":
+        bill += 3
+if extra_cheese == "Y":
+    bill += 1
 
-# elif size != "M":
-# elif "L" != size
-
-
-# elif size ==M
-
-# bill=20
-
-# elif size = L
-
-# bill= 25
-
-
-# add_pepperoni = input("Do you want pepperoni ? Y or N")
-
-# if add_pepperoni == "Y"
-   # bill+=3
-
-# extra_cheese = input("Do you want extra cheese ? Y or N")
-
-# if extra_cheese == "Y"
-# bill+=1
-
-print(f"Your final bill is :${bill} ")
+print(f"Your final bill is: ${bill}.")
