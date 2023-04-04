810b928fa159bc8fd4bfb59370e450d41901de59
il <ilya.mv@gmail.com>
2023-04-04 08:19:08 -0400

    love_calculator

---
+++ udemy/day_3/day_3EX_LoveCalculator.py
@@ -0,0 +1,28 @@
+print("Welcome to the the Love Calculator!")
+
+name1 = input("What is your name ? \n")
+
+name2 = input("What is their name ? \n")
+
+combined = name1 + name2
+print(combined)
+
+lower_case = combined.lower()
+
+lower_case.count("t")
+lower_case.count("r")
+lower_case.count("u")
+lower_case.count("e")
+
+lower_case.count("l")
+lower_case.count("o")
+lower_case.count("v")
+lower_case.count("e")
+
+
+
+#lower_case_name = name1.lower()
+#lower_case_name.count("true")
+#lower_case_name.count("love")
+
+
