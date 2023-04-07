b1548f7a62c325c9c343cf4ba8b2e146f35347a2
il <ilya.mv@gmail.com>
2023-04-06 22:00:38 -0400

    Pizza_Calculator

--- udemy/day_3/day_3EX_LoveCalculator.py
+++ udemy/day_3/day_3EX_LoveCalculator.py
@@ -1,23 +1,42 @@
 print("Welcome to the the Love Calculator!")
-
 name1 = input("What is your name ? \n")
-
 name2 = input("What is their name ? \n")
 
-combined = name1 + name2
-print(combined)
+combined_names = name1 + name2
+lower_case_names = combined_names.lower()
+letter_count = 0
+word_true = "true"
+for letter in word_true:
+    true_counter = letter_count + lower_case_names.count(letter)
 
-lower_case = combined.lower()
+word_love = "love"
+letter_count = 0
+for letter in word_love:
+    love_counter = letter_count + lower_case_names.count(letter)
+digits_counter = f"{true_counter}{love_counter}"
+print(digits_counter)
 
-lower_case.count("t")
-lower_case.count("r")
-lower_case.count("u")
-lower_case.count("e")
+# lower_case.count("u")
+# lower_case.count("e")
+#
+# true = t + r + u +e
+#
+# lower_case.count("l")
+# lower_case.count("o")
+# lower_case.count("v")
+# lower_case.count("e")
+#
+# love = l + o + v + e
+#
+# love_score = str(true) + str(love)
+#
+# print("your score is x , you go together like coke and mentos ")
+#
+# print("your score is y, you are alright together ")
 
-lower_case.count("l")
-lower_case.count("o")
-lower_case.count("v")
-lower_case.count("e")
+#print("your score is z ")
+
+
 
 
 
