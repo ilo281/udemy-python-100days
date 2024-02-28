8649f4c51f84a81ae49c8c43fd7cfb2218796b40
il <ilya.mv@gmail.com>
2024-02-27 19:52:57 -0500

    completed edabit 3,4

---
+++ edabit/edabit_2_inter_pass.py
@@ -0,0 +1,9 @@
+def addition(num):
+    this_sum = num + 1
+
+    return this_sum
+
+
+print(addition(5))
+print(addition(9))
+print(addition(-3))
---
+++ edabit/edadbit_1_addition.py
@@ -0,0 +1,8 @@
+def addition(a, b):
+    this_sum = a + b
+    return this_sum
+
+
+addition(5, 6)
+
+print(addition(5, 6))
