4bf868b3300dfad0504ccbfa5ae3feee71d68eb4
ilo281 <ilya.mv@gmail.com>
2024-09-03 19:54:37 -0400

    Completed 35 Vowel Replacer

--- edabit/edabit_35_vowel_replacer.py
+++ edabit/edabit_35_vowel_replacer.py
@@ -1,15 +1,15 @@
-def replace_vowels(input_string, ch):
+def replace_vowels(input_string, ch): # declared  function
 
     # 1 find vowels
     # 2 replace with specified characters
 
-    list_of_vowels = ["a", "e", "i", "o", "u"]
-    new_list = list(input_string)
-    for letter in new_list:
-        if letter in list_of_vowels:
-            this_index = new_list.index(letter)
-            new_list[this_index] = ch
-    string_to_return = "".join(new_list)
+    list_of_vowels = ["a", "e", "i", "o", "u"] # created list of vowels
+    new_list = list(input_string) # declared variable which creates list from string
+    for letter in new_list: # loop through list
+        if letter in list_of_vowels: # look for vowel in  the list created after loop
+            this_index = new_list.index(letter) # determine index of a letter  in new_list
+            new_list[this_index] = ch # replace a character with # based on index
+    string_to_return = "".join(new_list) # convert list to string
 
     return string_to_return
 
