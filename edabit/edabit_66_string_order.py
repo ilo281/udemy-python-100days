aeed4e5cbc228e0e54935cabe60a8b95f39ef1f3
ilo281 <ilya.mv@gmail.com>
2025-07-08 20:42:00 -0400

    edabit 66 string in order -
    Added Class + Assertions

--- edabit/edabit_66_string_order.py
+++ edabit/edabit_66_string_order.py
@@ -1,20 +1,24 @@
 # Create a function that takes a string and returns True or False,
 # If characters in order return  True if not return False
 
-def is_in_order(input_string):
-    ordered_string = False
-    sorted_input_string = sorted(input_string)
-    print("sorted_input_string", sorted_input_string)
-    input_string_as_list = []
-    for i in input_string:
-        input_string_as_list.append(i) # append function convert string to list
-    print("input_string_as_list", input_string_as_list)
-    if input_string_as_list == sorted_input_string:
-        ordered_string = True
-    return ordered_string
+class Order:
+    def is_in_order(self, input_string):
+        ordered_string = False
+        sorted_input_string = sorted(input_string)
+        input_string_as_list = []
+        for i in input_string:
+            input_string_as_list.append(i) # append function convert string to list
+        if input_string_as_list == sorted_input_string:
+            ordered_string = True
+
+        return ordered_string
+    def mike(self):
+        return "steve."
 
 
-print(is_in_order("abc"))
-print(is_in_order("edabit"))
-print(is_in_order("123"))
-print(is_in_order("xyzz"))
+order_object = Order() # creating instance of a class
+assert order_object.mike() == "steve." # assertions are units tests
+assert order_object.is_in_order("abc")
+assert not order_object.is_in_order("edabit") # not is meaning not equal to
+assert order_object.is_in_order("123")
+assert order_object.is_in_order("xyzz")
